22e811e2152349c78bb63303d8177bfb3fde9933
seremba john <serembaj@gmail.com>
2019-07-07 13:33:21 +0300

    Create layout for weather forecast

---
+++ app/src/main/java/com/andela/bookmarkit/ui/cities/SearchFragment.java
@@ -0,0 +1,50 @@
+package com.andela.bookmarkit.ui.cities;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.andela.bookmarkit.R;
+import com.andela.bookmarkit.ui.base.BaseFragment;
+
+
+public class SearchFragment extends BaseFragment {
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    public static SearchFragment newInstance() {
+        return new SearchFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initUI();
+    }
+
+    private void initUI() {
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+}
